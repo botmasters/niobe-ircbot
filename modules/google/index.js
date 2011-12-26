741b361de05d0d5c589d65793b81d4879719ecd6
zephrax <zephrax@gmail.com>
2011-12-26 15:44:12 -0300

    google fix

--- modules/google/index.js
+++ modules/google/index.js
@@ -36,10 +36,15 @@ var Google = {
     cmdGoogle : function (server, from, target, message) {
         request('https://ajax.googleapis.com/ajax/services/search/web?' + querystring.stringify({v: '1.0', q: message}), function (error, response, body) {
             if (!error && response.statusCode == 200) {
-                var result = JSON.parse(body);
-                for (var t=0;t<3;t++) {
-                    var item = result.responseData.results[t];
-                    googleModule.bot.clients[server].say(target, item.titleNoFormatting + '(' + item.unescapedUrl + ')');
+                var result = JSON.parse(body),
+                    count = result.responseData.results.length >= 3 ? 3 : result.responseData.results.length;
+                if (count > 0) {
+                    for (var t=0;t<count;t++) {
+                        var item = result.responseData.results[t];
+                        googleModule.bot.clients[server].say(target, item.titleNoFormatting + '(' + item.unescapedUrl + ')');
+                    }
+                } else {
+                    googleModule.bot.clients[server].say(target, 'No results found');
                 }
             } else {
                 googleModule.bot.clients[server].say(target, 'Error sending request. Status Code #' + response.statusCode);
